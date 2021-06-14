6695467950d3d3209ecb7eeeb3ce1c6900cc273e
Lance Whatley <whatl3y@gmail.com>
2021-06-14 10:33:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# moontography-api
+
+### Development
+
+```sh
+$ git clone https://github.com/moontography/moontography-api
+$ cd moontography-api
+$ npm install
+
+# run tests
+$ npm test
+
+# build production files
+$ npm run build
+
+# start server
+$ npm start
+```
+
+### License
+
+MIT
---
+++ src/config.ts
@@ -0,0 +1,24 @@
+const appName = process.env.APP_NAME || "moontography-api";
+
+export default {
+  app: {
+    name: appName,
+  },
+
+  server: {
+    // isProduction: process.env.NODE_ENV === 'production',
+    port: process.env.PORT || 8080,
+  },
+
+  logger: {
+    options: {
+      name: appName,
+      streams: [
+        {
+          level: process.env.LOGGING_LEVEL || "info",
+          stream: process.stdout,
+        },
+      ],
+    },
+  },
+};
